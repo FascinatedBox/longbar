d608084ca6432bf10b09f78eeb0853474adf1f20
FascinatedBox <FascinatedBox@protonmail.com>
2024-06-28 13:24:06 -0700

    Warn the user instead of starting if unable to find a Wayland display.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <gdk/gdkx.h>
 #include <gtk-layer-shell.h>
 #include <gtk/gtk.h>
 #include <stdio.h>
@@ -5,7 +6,20 @@
 #include "dbus-dispatch.h"
 #include "longbar.h"
 
+static void ensure_wayland_display(void) {
+  GdkDisplay *display = gdk_display_get_default();
+
+  // GDK_IS_WAYLAND_DISPLAY generates warnings if given an X11 display, but the
+  // reverse is not true, so use GDK_IS_X11_DISPLAY instead.
+  if (GDK_IS_X11_DISPLAY(display)) {
+    fputs("longbar: Cannot connect to a Wayland display. Stopping.\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+}
+
 static void activate(GtkApplication *app) {
+  ensure_wayland_display();
+
   Longbar *bar = longbar_new(app);
 
   long_register_for_dbus(bar);
@@ -14,6 +28,10 @@ static void activate(GtkApplication *app) {
 }
 
 int main(int argc, char **argv) {
+  // If X11 is not included as a fallback, the user will see 'cannot open
+  // display :0.0'. By adding it as a fallback, a more useful error message can
+  // be written later.
+  gdk_set_allowed_backends("wayland,x11");
   GtkApplication *app = gtk_application_new("com.fascinatedbox.longbar",
                                             G_APPLICATION_FLAGS_NONE);
 
